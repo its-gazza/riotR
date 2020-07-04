4733f2e264f24b9fbd75e8c694fb0590a6894e7d
its-gazza <gary.lkw95@gmail.com>
2020-07-04 18:50:42 +1000

    Added roxygen documentation

--- R/misc.R
+++ R/misc.R
@@ -1,9 +1,12 @@
-paste_url <- function(end_point, region) {
-  base_url <- ""
-}
-
 # Glue URL
-glue_url = function(url, api, dry_run = FALSE) {
+#' Title
+#'
+#' @param url URL end point
+#' @param api Riot API
+#' @param dry_run If True return url
+#'
+#' @export
+get_url = function(url, api, dry_run = FALSE) {
   url <- httr::modify_url(
     url = glue(url),
     query = list(api_key = api)
@@ -13,14 +16,18 @@ glue_url = function(url, api, dry_run = FALSE) {
   if (dry_run) {
     return(url)
   } else {
-    httr::GET(url) %>%
-      return()
+    df <- httr::GET(url)
   }
 }
 
+#' Route TFT region
+#'
+#' @param region region
+#'
+#' @export
 route_tft <- function(region) {
   region <- tolower(region)
-  if (str_detect(region, "(oc)|(na)|(br)|(lan)|(las)")) {
+  if (str_detect(region, "(oc)|(na)|(br)|(la)|(las)")) {
     output <- "americas"
   } else if (str_detect(region, "(kr)|(jp)")) {
     output <- "asia"
@@ -32,3 +39,59 @@ route_tft <- function(region) {
 
   return(output)
 }
+
+#' Check tier input
+#'
+#' @param tier Input for tier
+#'
+#' @export
+check_tier <- function(tier) {
+  tier <- toupper(tier)
+
+  if (tier == "DIAMOND" || tier == "PLATINUM" || tier == "GOLD" ||
+      tier == "SILVER" || tier == "BRONZE" || tier == "IRON") {
+    return(tier)
+  } else {
+    warning(glue::glue("Incorrect tier, got {tier},
+                       expected: DIAMOND, PLATINUM, GOLD, SILVER, BRONZE, IRON"))
+  }
+}
+
+#' Check division input
+#'
+#' @param division Input for division
+#'
+#' @export
+check_division <- function(division) {
+  # If input is numeric then do conversion
+  if(!is.character(division)) {
+    division <- switch(
+      division,
+      "I",
+      "II",
+      "III",
+      "IV"
+    )
+
+    # If input is out of range then it'll return NULL
+    if (is.null(division)) {
+      warning("Incorrect input")
+    }
+  }
+
+  # Check if input is one of the 4 division
+  if (division == "I" || division == "II" ||
+      division == "III"|| division == "IV") {
+    return(division)
+  } else {
+    warning("Incorrect input")
+  }
+
+}
+
+
+
+
+
+
+
